5161e21c0ce272050d577a670282a14d4562d10a
L33T <271813410+chiefbigrubberduck@users.noreply.github.com>
2026-04-07 14:19:59 +0200

    Update macchangerscript.sh
    Added info into .sh

--- macchangerscript.sh
+++ macchangerscript.sh
@@ -9,4 +9,5 @@ sudo ip link set "$INTERFACE" down
 sudo macchanger -b -r "$INTERFACE"
 
 # 3. OPTIONAL: Bring the interface back UP, disabled for security reasons
+# 4. The -r switch makes it a completely random, and the -b switch makes it look as if the device originated with the address macchanger is giving it, and was never changed. 
 
