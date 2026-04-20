70723090283079e67e85555b399d9fcd2e20658e
L33T <271813410+chiefbigrubberduck@users.noreply.github.com>
2026-04-20 15:29:59 +0200

    Update macchangerscript.sh

--- macchangerscript.sh
+++ macchangerscript.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-INTERFACE="YOUR WIFI ADAPTER NAME!!!!"
+INTERFACE="YOUR WIFI ADAPTER NAME"
 
 # 1. Bring the interface DOWN
 sudo ip link set "$INTERFACE" down
 
 # 2. Change the MAC
 sudo macchanger -b -r "$INTERFACE"
+
+echo "                                                                                                                             "
+echo "The script is finished running, you will have to turn your Wi-Fi back on.)
+echo "Check the Github page for updates!"
 
 # 3. OPTIONAL: Bring the interface back UP, disabled for security reasons
 # 4. The -r switch makes it a completely random, and the -b switch makes it look as if the device originated with the address macchanger is giving it, and was never changed. 
